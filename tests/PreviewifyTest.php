0bcd0147451dc37242dd4e17407dfbed4a67d0f4
Logan Craft <hello@logancraft.dev>
2022-12-08 21:14:07 -0500

    Fix test.

--- tests/PreviewifyTest.php
+++ tests/PreviewifyTest.php
@@ -13,7 +13,7 @@ it('can generate an async image', function () {
     ]);
 
     assertEquals(
-        'https://previewlinks.io/api/async-image?data=eyJ0ZW1wbGF0ZV9pZCI6MSwiZmllbGRzIjp7InByZXZpZXdpZnk6dGl0bGUiOiJIaSIsInByZXZpZXdpZnk6ZGVzY3JpcHRpb24iOiJUaGlzIGlzIGEgdGVzdCJ9fQ%3D%3D&signature=276956e53123b06bcf6a71624eb9a69a7e32cbfa72281ad53601fb24ae2468b8',
+        'https://previewlinks.io/api/async-image?data=eyJ0ZW1wbGF0ZV9pZCI6MSwiZmllbGRzIjp7InByZXZpZXdsaW5rczp0aXRsZSI6IkhpIiwicHJldmlld2xpbmtzOmRlc2NyaXB0aW9uIjoiVGhpcyBpcyBhIHRlc3QifX0%3D&signature=8f690cf87aa9994a42eb7d4920788c6643a1cc9ec0025ec77a912052921af1f6',
         $url,
     );
 });
